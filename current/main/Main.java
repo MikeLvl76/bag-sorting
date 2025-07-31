834347656407d865e9a3f0dc42cac2d4eb0a8242
Mickael Louvel <mickael.louvel@etu.univ-lehavre.fr>
2025-07-31 18:53:34 +0200

    make class for 3D shape and make interface to calculate volume for shapes

--- current/main/Main.java
+++ current/main/Main.java
@@ -2,11 +2,17 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        Map<String, Double> map = Map.of("width", 2.0, "length", 6.0);
-        Shape2D square = new Shape2D("square", map);
-        System.out.println(square.getAttributes());
-        square.calculateArea();
-        System.out.println("Area of square: " + square.getArea());
+        // Map<String, Double> map = Map.of("width", 2.0, "length", 6.0);
+        // Shape2D square = new Shape2D("square", map);
+        // System.out.println(square.getAttributes());
+        // square.calculateArea();
+        // System.out.println("Area of square: " + square.getArea());
+
+        Map<String, Double> map = Map.of("width", 2.0, "length", 6.0, "depth", 2.4);
+        Shape3D rect = new Shape3D("rect", map);
+        System.out.println(rect.getAttributes());
+        rect.calculateVolume();
+        System.out.println("Volume of rect: " + rect.getVolume());
         System.out.println("TODO: Main class");
     }
 }
--- current/main/Shape3D.java
+++ current/main/Shape3D.java
@@ -1,56 +1,69 @@
 import java.util.Map;
 
-public class Shape3D extends Shape {
-    private double area;
+public class Shape3D extends Shape implements VolumeCalculator {
+    private double volume;
 
     public Shape3D(String _name, Map<String, Double> map) {
         this.setName(_name);
         this.setDimension(3);
         this.setCanStack(false);
         this.updateAttributes(map);
-        this.area = 0.0;
+        this.volume = 0.0;
     }
 
-    public double getArea() {
-        return this.area;
+    public double getVolume() {
+        return this.volume;
     }
 
-    public void calculateArea() {
+    public void calculateVolume() {
         switch (this.name) {
-            case "square":
-                
+            case "cube":
+                double length = this.getAttributeValue("length");
+                this.volume = getCubeVolume(length);
                 break;
 
-            case "triangle":
-                
+            case "rect":
+                double width = this.getAttributeValue("width");
+                double _length = this.getAttributeValue("length");
+                double depth = this.getAttributeValue("depth");
+                this.volume = getRectVolume(width, _length, depth);
                 break;
 
-            case "circle":
-                
-                break;
-
-
-            case "trapezium":
-                
-                break;
-
-
-            case "parallelogram":
-                
+            case "cylinder":
+                double radius = this.getAttributeValue("radius");
+                double height = this.getAttributeValue("height");
+                this.volume = getCylinderVolume(radius, height);
                 break;
 
 
-            case "rhombus":
-                
+            case "cone":
+                double r = this.getAttributeValue("radius");
+                double h = this.getAttributeValue("height");
+                this.volume = getConeVolume(r, h);
                 break;
 
 
-            case "polygon":
-                
+            case "sphere":
+                double _radius = this.getAttributeValue("radius");
+                this.volume = getSphereVolume(_radius);
+                break;
+
+
+            case "pyramid":
+                double baseArea = this.getAttributeValue("base-area");
+                double _h = this.getAttributeValue("height");
+                this.volume = getPyramidVolume(baseArea, _h);
+                break;
+
+
+            case "prism":
+                double polygonBaseArea = this.getAttributeValue("polygon-base-area");
+                double _height = this.getAttributeValue("height");
+                this.volume = getPrismVolume(polygonBaseArea, _height);
                 break;
         
             default:
-                this.area = 0.0;
+                this.volume = 0.0;
                 break;
         }
     }
