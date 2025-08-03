9e5a7f76da8970cb04c5e227e6316ca7f3505cff
Mickael Louvel <mickael.louvel@etu.univ-lehavre.fr>
2025-08-03 14:37:00 +0200

    remove useless sysout and improve a bit display

--- current/main/Bag.java
+++ current/main/Bag.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.stream.Stream;
 
 public class Bag {
     private ArrayList<Shape> content;
@@ -49,6 +50,10 @@ public class Bag {
         return column;
     }
 
+    public long count() {
+        return this.content.stream().filter(shape -> shape != null).count();
+    }
+
     private Boolean hasColumnFull(int colIndex) {
         ArrayList<Shape> column = this.getColumn(colIndex);
         return column.stream().allMatch(shape -> shape != null);
@@ -69,73 +74,10 @@ public class Bag {
         }
     }
 
-    // public void insert(Shape ...shapes) {
-    //     if (shapes.length == 0) return;
-    //     if (this.isFull()) return;
-
-    //     int shapeIndex = 0;
-    //     int contentIndex = this.content.size() - 1;
-
-    //     while(contentIndex > -1) {
-    //         if (shapeIndex == shapes.length) break;
-
-    //         int nextIndex = contentIndex - 1;
-    //         Shape current = this.content.get(contentIndex);
-    //         Shape shape = shapes[shapeIndex];
-
-    //         // Place is not taken by another shape
-    //         if (current == null) {
-    //             this.content.set(contentIndex, shape);
-    //             shapeIndex++;
-    //             contentIndex--;
-    //             continue;
-    //         }
-
-    //         // Place is taken
-    //         // Compare dimension between current shape and the new
-    //         if (current.getDimension() < shape.getDimension()) {
-    //             // We consider that a 3D shape cannot stack with a 2D shape if the second one is below
-    //             contentIndex--;
-    //             shapeIndex++;
-    //             continue;
-    //         } else if (current.getDimension() > shape.getDimension()) {
-    //             // The opposite is true only if the volume of the 3D shape is twice greater than the area of the 2D shape
-    //             double volume = current.getAttributeValue("volume");
-    //             double area = shape.getAttributeValue("area");
-    //             if (volume > 2 * area) {
-    //                 this.content.set(nextIndex, shape);
-    //                 contentIndex--;
-    //                 shapeIndex++;
-    //                 continue;
-    //             }
-    //         } else if (current.getDimension() == shape.getDimension()) {
-    //             // If both are 2D shapes then only if the shape below has a greater area than the other
-    //             if (current.getDimension() == 2) {
-    //                 double currentArea = current.getAttributeValue("area");
-    //                 double shapeArea = shape.getAttributeValue("area");
-    //                 if (currentArea > shapeArea) {
-    //                     this.content.set(nextIndex, shape);
-    //                     contentIndex--;
-    //                     shapeIndex++;
-    //                     continue;
-    //                 }
-    //             }
-    //             // Same for 3D shapes
-    //             double currentVolume = current.getAttributeValue("volume");
-    //             double shapeVolume = shape.getAttributeValue("volume");
-    //             if (currentVolume > shapeVolume) {
-    //                 this.content.set(nextIndex, shape);
-    //                 contentIndex--;
-    //                 shapeIndex++;
-    //                 continue;
-    //             }
-    //         }
-    //     }
-    // }
-
-    public Shape[] insertIntoColumn(int colIndex, Shape ...shapes) {
-        if (colIndex < 0 || colIndex >= this.stackCount) return new Shape[0];
-        if (this.hasColumnFull(colIndex) || shapes.length == 0) return new Shape[0];
+    private Shape[] insertIntoColumn(int colIndex, Shape ...shapes) {
+        if (this.isFull()) return shapes;
+        if (colIndex < 0 || colIndex >= this.stackCount) return shapes;
+        if (this.hasColumnFull(colIndex) || shapes.length == 0) return shapes;
 
         ArrayList<Shape> notInserted = new ArrayList<>();
         ArrayList<Shape> column = this.getColumn(colIndex);
@@ -154,7 +96,6 @@ public class Bag {
             // Case 1: empty column
             if (previous == null) {
                 column.set(previousIndex, shape);
-                System.out.println("First element: " + column.get(previousIndex));
                 shapeIndex++;
                 continue;
             }
@@ -167,9 +108,6 @@ public class Bag {
                 // then we had the new to the list of non-inserted shapes and go to
                 // next shape
                 if (previous.getDimension() < shape.getDimension()) {
-                    System.out.println("Case 1");
-                    System.out.println("Previous: " + previous);
-                    System.out.println("Shape: " + shape);
                     notInserted.add(shape);
                     shapeIndex++;
                     continue;
@@ -179,9 +117,6 @@ public class Bag {
                 // if the volume of the previous one is twice bigger than the area of the
                 // new, then we can insert it and go to next shape and go to next empty place
                 if (previous.getDimension() > shape.getDimension()) {
-                    System.out.println("Case 2");
-                    System.out.println("Previous: " + previous);
-                    System.out.println("Shape: " + shape);
                     double volume = previous.getAttributeValue("volume");
                     double area = shape.getAttributeValue("area");
                     if (volume > 2 * area) {
@@ -199,9 +134,6 @@ public class Bag {
                 // if the area or volume of the previous one is bigger than the area or the volume of the
                 // new, then we can insert it and go to next shape and go to next empty place
                 if (previous.getDimension() == shape.getDimension()) {
-                    System.out.println("Case 3");
-                    System.out.println("Previous: " + previous);
-                    System.out.println("Shape: " + shape);
                     int dim = previous.getDimension();
                     if (dim == 2) {
                         double previousArea = previous.getAttributeValue("area");
@@ -231,54 +163,22 @@ public class Bag {
             continue;
         }
         updateColumn(colIndex, column);
-        Shape[] result = notInserted.toArray(new Shape[notInserted.size()]);
+        Shape[] notInsertedArray = notInserted.toArray(new Shape[notInserted.size()]);
         if (notInserted.size() > 0) {
-            return this.insertIntoColumn(colIndex - 1, result);
+            return this.insertIntoColumn(colIndex - 1, notInsertedArray);
         }
-        return result;
+        return notInsertedArray;
     }
 
     public void insert(Shape ...shapes) {
         int colIndex = this.stackCount() - 1;
         Shape[] notInserted = this.insertIntoColumn(colIndex, shapes);
-        System.out.println(Arrays.toString(notInserted));
+        System.out.println(String.format("Remaining shapes: %s\n", Arrays.toString(notInserted)));
     }
 
     public void displayColumn(int colIndex) {
         ArrayList<Shape> column = this.getColumn(colIndex);
         System.out.println(column.stream().map(shape -> shape == null ? "x" : shape.minimalDisplay()).toList());
-    }
-
-    // public void insert(Shape ...shapes) {
-    //     if (shapes.length == 0) return;
-    //     if (this.isFull()) return;
-
-    //     int index = this.content.size() - 1;
-    //     for (Shape shape : shapes) {
-    //         if (this.content.isEmpty()) {
-    //             this.content.set(index, shape);
-    //             continue;
-    //         }
-
-
-    //     }
-    // }
-
-    public void insertInto(Shape shape) {
-        if (this.content.isEmpty()) {
-            this.content.set(this.content.size() - 1, shape);
-            return;
-        }
-
-        int index = this.content.size() - 1;
-        while(index > 0) {
-            if (this.content.get(index) != null) {
-                index--;
-            } else {
-                this.content.set(index, shape);
-                break;
-            }
-        }
     }
 
     public String toString() {
@@ -304,6 +204,7 @@ public class Bag {
             }
         }
     
+        builder.append(String.format("\nNumber of shapes: %d", this.count()));
         return builder.toString();
     }
     
--- current/main/Main.java
+++ current/main/Main.java
@@ -1,35 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        // Map<String, Double> map = Map.of("width", 2.0, "length", 6.0);
-        // Shape2D square = new Shape2D("square", map);
-        // System.out.println(square.getAttributes());
-        // square.calculateArea();
-        // System.out.println("Area of square: " + square.getArea());
-
-        // Map<String, Double> map3D = Map.of("width", 2.0, "length", 6.0, "depth", 2.4);
-        // Shape3D rect = new Shape3D("rect", map3D);
-        // System.out.println(rect.getAttributes());
-        // rect.calculateVolume();
-        // System.out.println("Volume of rect: " + rect.getVolume());
-
         Bag bag = new Bag(16, 4);
         ShapeGenerator generator = new ShapeGenerator();
-        generator.shapes(4);
+        int number = 12;
+        generator.shapes(number);
 
         System.out.println(bag);
-        // System.out.println(generator.getShapes());
-
-        bag.insert(generator.getShapes().toArray(new Shape[4]));
-
-        // for (Shape shape : generator.getShapes()) {
-        //     //bag.insertInto(shape);
-        //     bag.insertIntoColumn(0, shape);
-        // }
-
-        // for (int i = 0; i < bag.stackCount(); i++){
-        //     bag.displayColumn(i);
-        // }
-
+        bag.insert(generator.getShapes().toArray(new Shape[number]));
         System.out.println(bag);
     }
 }
